7a2175089c465bcfef305c6349cebf6880a69c0e
Ashiq Ali <ashiq.liaqat465@gmail.com>
2022-05-20 17:19:47 +0500

    fixed isuues

--- backend/db.js
+++ backend/db.js
@@ -2,15 +2,15 @@ const mongoose = require("mongoose");
 
 function connectDB() {
   // mongoose.connect('mongodb+srv://athif:Ryzenathif@cluster0.t5pyu.mongodb.net/athifcars' , {useUnifiedTopology: true , useNewUrlParser: true})
-  // mongoose.connect("mongodb+srv://ashiqali:ashiq12345@cluster0.uii8z.mongodb.net/BikeRental?retryWrites=true&w=majority", {
-  //   useUnifiedTopology: true,
-  //   useNewUrlParser: true,
-  // });
-
- mongoose.connect('mongodb://127.0.0.1:27017/BikeRental', {
+  mongoose.connect("mongodb+srv://ashiqali:ashiq12345@cluster0.uii8z.mongodb.net/BikeRental?retryWrites=true&w=majority", {
     useUnifiedTopology: true,
     useNewUrlParser: true,
   });
+
+//  mongoose.connect('mongodb://127.0.0.1:27017/BikeRental', {
+//     useUnifiedTopology: true,
+//     useNewUrlParser: true,
+//   });
 
   'mongodb://127.0.0.1:27017'
   const connection = mongoose.connection;
